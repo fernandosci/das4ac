43a47755e309c4dfb6825c92f4841ab2952907e6
fernandosci <fernandosci@gmail.com>
2014-12-01 22:38:32 +0000

    C

--- tests/test2_auctionSystem.sh
+++ tests/test2_auctionSystem.sh
@@ -3,15 +3,26 @@ export SERVERSH=server.sh
 
 export TEST=1
 
-export SPARAMS="1 60 5 1000"
+export SPARAMS="1 4 5 1000"
 
 cd ..
 
-sleep 6s && xterm -e sh ./client.sh "127.0.0.1 userlogin ${TEST} 500" &
+#CREATOR
+export TEST=2
+sleep 5s && xterm -e sh ./client.sh "127.0.0.1 CREATOR ${TEST} 1" &
 
-for ((i=0; i<=0; i++)); do
-sleep 5s && xterm -e sh ./client.sh "127.0.0.1 userlogin_$i ${TEST} 1" &
-done
+#BIDDER
+export TEST=3
+sleep 5s && xterm -e sh ./client.sh "127.0.0.1 BIDDER ${TEST} 2" &
+
+#LISTENER
+export TEST=4
+sleep 5s && xterm -e sh ./client.sh "127.0.0.1 LISTENER ${TEST} 1" &
+
+#HISTORIAN
+export TEST=5
+sleep 5s && xterm -e sh ./client.sh "127.0.0.1 HISTORIAN ${TEST} 1" &
+
 
 sh ./server.sh $SPARAMS
 
